e3bdb0ab000de27e84c739ecd0bf54cc54712389
Micha Ivanchenko <mykhailo.ivanchenko@netways.de>
2025-07-22 13:58:15 +0200

    Add the 1.2 task for points in 3d

---
+++ pointsin3d.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+struct Point{
+    double x, y, z;
+};
+
+double getDistance(Point a, Point b) {
+    return sqrt(pow(b.x-a.x,2)+pow(b.y-a.y,2)+pow(b.z-a.z,2));
+}
+
+int main(){
+    Point a, b;
+
+    a.x = 0;
+    a.y = 0;
+    a.z = 0;
+
+    b.x = 1;
+    b.y = 5;
+    b.z = 0;
+
+    cout << "oa" << &a << endl;
+    cout << "ob" << &b << endl;
+    
+    cout << "Distance between points is: " << getDistance(a, b) << "\n";
+
+    return 0;
+}
